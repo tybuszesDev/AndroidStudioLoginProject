40f0a11decbd085a82737e82bb50af93cac219cc
tybuszes <tybuszes@protonmail.com>
2019-12-02 03:03:02 +0100

    dodano menu

--- app/src/main/java/com/example/mnygrower/SecondActivity.java
+++ app/src/main/java/com/example/mnygrower/SecondActivity.java
@@ -1,17 +1,23 @@
 package com.example.mnygrower;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class SecondActivity extends AppCompatActivity {
 
     Button logout;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +26,38 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         logout = findViewById(R.id.btnLogout);
+        firebaseAuth = FirebaseAuth.getInstance();
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SecondActivity.this, MainActivity.class));
-                Toast.makeText(SecondActivity.this, "LogOut Successful", Toast.LENGTH_SHORT).show();
+                Logout();
             }
         });
 
+    }
+
+    private void Logout(){
+        firebaseAuth.signOut();
+        finish();
+        startActivity(new Intent(SecondActivity.this, MainActivity.class));
+        Toast.makeText(SecondActivity.this, "LogOut Successful", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.logoutMenu:{
+                Logout();
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
